07ee0143dba94fd85d65744d0f30070833d37fbb
Charl <meyers.charl@gmail.com>
2018-03-19 21:04:44 +0200

    Remove email body when anonomizing

--- testFolderSearch.py
+++ testFolderSearch.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python
 import os
 import shutil
+import sys
+
+def removeBody(infile):
+	emailHeaderLines = []
+	for line in infile:
+		if line != "\n":
+			emailHeaderLines.append(line)
+		else:
+			return emailHeaderLines
 
 
-def search_directory(directory, outputDirectory):	
-	for root, directories, files in os.walk(directory):		
+def search_directory(directory, outputDirectory):
+	for root, directories, files in os.walk(directory):
 		outputPath = os.path.join(outputDirectory, root[len(directory)+1:])		
 		if not os.path.isdir(outputPath):
-			os.mkdir(outputPath)
-		print("Anonymising ", root)		
+			os.mkdir(outputPath)		
+		print("Anonymising ", root, end="\r")
 		for file in os.listdir(root):
 			filePath = os.path.join(root,file)
 			outputFilePath = os.path.join(outputPath,file)
 			if os.path.isfile(filePath):
-				with open(filePath, "r") as fileToCopy:
-					fileToCopyContent = fileToCopy.read()
-					# with open(outputFilePath, "w+") as file_to_write:
-					# 	file_to_write.write(fileToCopyContent)
-					print(fileToCopy.find("\n\r"))
-		
+				with open(filePath, "r") as fileToCopy:					
+					with open(outputFilePath, "w+") as fileToWrite:
+						removedBodyFromEmail = removeBody(fileToCopy)
+						fileToWrite.writelines(removedBodyFromEmail)
+
 
 
 
